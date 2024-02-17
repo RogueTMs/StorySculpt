9a52d23027ff1f3c6de4bd479f9f5079095abf26
dRamee <maksim.yolckin@yandex.com>
2024-02-18 02:07:51 +0700

    So Good prompt

--- StorySculpt/Session.cs
+++ StorySculpt/Session.cs
@@ -1,11 +1,5 @@
 ﻿using StorySculpt.Generators;
 using StorySculpt.TelegramImpl;
-using Telegram.Bot;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using StorySculpt.Classes;
 
 namespace StorySculpt
